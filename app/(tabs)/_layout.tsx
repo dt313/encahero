01b8b4e0b90090ab83b83c11907b4eaaa80d91b2
dt313 <dt313.dev21@gmail.com>
2025-08-07 00:04:18 +0900

    chore: custome tab

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -4,10 +4,14 @@ import { Platform } from 'react-native';
 
 import { Tabs } from 'expo-router';
 
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+
 import { Colors } from '@/constants/Colors';
 
 import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
 import TabBarBackground from '@/components/ui/TabBarBackground';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
@@ -35,35 +39,37 @@ export default function TabLayout() {
                 name="index"
                 options={{
                     title: 'Home',
-                    tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+                    tabBarIcon: ({ color }) => {
+                        return <FontAwesome5 name="home" size={24} color={color} />;
+                    },
                 }}
             />
             <Tabs.Screen
                 name="list"
                 options={{
                     title: 'List',
-                    tabBarIcon: ({ color }) => <IconSymbol size={28} name="rectangle.stack.fill" color={color} />,
+                    tabBarIcon: ({ color }) => <FontAwesome5 name="list-alt" size={24} color={color} />,
                 }}
             />
             <Tabs.Screen
                 name="quiz"
                 options={{
                     title: 'Quiz',
-                    tabBarIcon: ({ color }) => <IconSymbol size={28} name="questionmark.circle.fill" color={color} />,
+                    tabBarIcon: ({ color }) => <MaterialIcons name="quiz" size={24} color={color} />,
                 }}
             />
             <Tabs.Screen
                 name="battle"
                 options={{
                     title: 'Battle',
-                    tabBarIcon: ({ color }) => <IconSymbol size={28} name="flame.fill" color={color} />,
+                    tabBarIcon: ({ color }) => <MaterialCommunityIcons name="axe-battle" size={24} color={color} />,
                 }}
             />
             <Tabs.Screen
                 name="profile"
                 options={{
                     title: 'Profile',
-                    tabBarIcon: ({ color }) => <IconSymbol size={28} name="person.crop.circle.fill" color={color} />,
+                    tabBarIcon: ({ color }) => <FontAwesome name="user-circle-o" size={24} color={color} />,
                 }}
             />
         </Tabs>
--- components/HapticTab.tsx
+++ components/HapticTab.tsx
@@ -1,18 +1,29 @@
+import React from 'react';
+
 import { BottomTabBarButtonProps } from '@react-navigation/bottom-tabs';
 import { PlatformPressable } from '@react-navigation/elements';
-import * as Haptics from 'expo-haptics';
+import Animated, { useAnimatedStyle, withSpring } from 'react-native-reanimated';
 
-export function HapticTab(props: BottomTabBarButtonProps) {
-  return (
-    <PlatformPressable
-      {...props}
-      onPressIn={(ev) => {
-        if (process.env.EXPO_OS === 'ios') {
-          // Add a soft haptic feedback when pressing down on the tabs.
-          Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
-        }
-        props.onPressIn?.(ev);
-      }}
-    />
-  );
-}
+export const HapticTab = ({ children, onPress, accessibilityState }: BottomTabBarButtonProps) => {
+    const focused = accessibilityState?.selected;
+
+    const animatedStyle = useAnimatedStyle(() => {
+        return {
+            transform: [
+                {
+                    scale: withSpring(focused ? 1.2 : 1),
+                },
+                {
+                    translateY: withSpring(focused ? -5 : 0),
+                },
+            ],
+            opacity: withSpring(focused ? 1 : 0.7),
+        };
+    }, [focused]);
+
+    return (
+        <PlatformPressable onPress={onPress} style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Animated.View style={animatedStyle}>{children}</Animated.View>
+        </PlatformPressable>
+    );
+};
